c814f3e2d0ec9644828b442a7b71932b68ecedd3
zusitools <zusitools@users.noreply.github.com>
2019-02-03 10:52:02 +0100

    Refactoring

--- radius_bogenweichen.cpp
+++ radius_bogenweichen.cpp
@@ -3,12 +3,13 @@
 
 #include <iostream>
 #include <unordered_map>
+#include <vector>
 
-using ElementUndRichtung = std::pair<StrElement*, bool>;
+using ElementUndRichtung = std::pair<const StrElement*, bool>;
 
 constexpr size_t WEICHE = 1 << 2;
 
-const std::unordered_map<std::string, std::string> weichen = {
+const std::unordered_map<std::string, std::string> OriginalWeichen = {
   {"54 500 1-12 Links", "PermanentWay\\Deutschland\\1435mm\\Regeloberbau\\Weichen\\500\\54 500 1-14 Links Holzschw K-Oberbau.st3"},
   {"54 500 1-12 Rechts", "PermanentWay\\Deutschland\\1435mm\\Regeloberbau\\Weichen\\500\\54 500 1-14 Links Holzschw K-Oberbau.st3"},
   {"54 500 1-12 Links", "PermanentWay\\Deutschland\\1235mm\\Regeloberbau\\Weichen\\500\\54 500 1-12 Links Holzschw K-Oberbau.st3"},
@@ -17,13 +18,17 @@ const std::unordered_map<std::string, std::string> weichen = {
   {"54 500 1-9 1-12 Rechts", "PermanentWay\\Deutschland\\1235mm\\Regeloberbau\\Weichen\\500\\54 500 1-9 1-12 Links Holzschw K-Oberbau.st3"},
 };
 
-int main(int argc, char* argv[]) {
-  const auto& zusi = zusixml::parseFile(argv[1]);
-  if (!zusi || !zusi->Strecke) {
-    return 1;
-  }
+struct Weiche {
+  const Signal* weichensignal;
+  ElementUndRichtung startElement;
+  std::vector<ElementUndRichtung> geraderStrang;
+  std::vector<ElementUndRichtung> abzweigenderStrang;
+};
 
-  const auto getNachfolger = [&str = zusi->Strecke](ElementUndRichtung el, size_t idx) -> ElementUndRichtung {
+std::vector<Weiche> FindeWeichen(const Strecke& str, bool nurBogenweichen = false) {
+  std::vector<Weiche> result;
+
+  const auto getNachfolger = [&str](ElementUndRichtung el, size_t idx) -> ElementUndRichtung {
     const auto& nachfolgerArray = (el.second ? el.first->children_NachNorm : el.first->children_NachGegen);
     const auto& anschlussMask = (el.second ? 0x1 : 0x100) << idx;
 
@@ -31,32 +36,31 @@ int main(int argc, char* argv[]) {
       return { nullptr, false };
     }
     const auto& nachfolgerNr = nachfolgerArray[idx].Nr;
-    if (nachfolgerNr < 0 || nachfolgerNr >= str->children_StrElement.size() || !str->children_StrElement[nachfolgerNr]) {
+    if (nachfolgerNr < 0 || nachfolgerNr >= str.children_StrElement.size() || !str.children_StrElement[nachfolgerNr]) {
       return { nullptr, false };
     }
-    return { str->children_StrElement[nachfolgerNr].get(), (el.first->Anschluss & anschlussMask) == 0 };
+    return { str.children_StrElement[nachfolgerNr].get(), (el.first->Anschluss & anschlussMask) == 0 };
   };
 
-  for (const auto& str_element : zusi->Strecke->children_StrElement) {
+  const auto folgeWeichenstrang = [&str, &getNachfolger](const ElementUndRichtung& el) -> std::vector<ElementUndRichtung> {
+    std::vector<ElementUndRichtung> result;
+    ElementUndRichtung cur = el;
+    while (cur.first && (cur.first->Fkt & WEICHE)) {
+      result.push_back(cur);
+      cur = getNachfolger(cur, 0);
+    }
+    return result;
+  };
+
+  for (const auto& str_element : str.children_StrElement) {
     if (!str_element) {
       continue;
     }
-
-    const auto folgeWeichenstrang = [&](const ElementUndRichtung& el) -> std::vector<ElementUndRichtung> {
-      std::vector<ElementUndRichtung> result;
-      ElementUndRichtung cur = el;
-      while (cur.first && (cur.first->Fkt & WEICHE)) {
-        result.push_back(cur);
-        cur = getNachfolger(cur, 0);
-      }
-      return result;
-    };
 
     if ((str_element->Fkt & WEICHE) &&
         (str_element->children_NachNorm.size() == 2 || str_element->children_NachGegen.size() == 2)) {
 
       const bool norm = (str_element->children_NachNorm.size() == 2);
-      std::cout << "\n" << str_element->Nr << "\n";
 
       const auto& richtungsInfo = (norm ? str_element->InfoNormRichtung : str_element->InfoGegenRichtung);
       assert(richtungsInfo.has_value());
@@ -67,42 +71,74 @@ int main(int argc, char* argv[]) {
 
       const auto& signalFrame = signal->children_SignalFrame[0];
 
-      std::cout << "Erstes Signalframe:\n";
-      std::cout << " - " << signalFrame->Datei.Dateiname << "\n";
-
-      const bool bogenweiche = (signalFrame->Datei.Dateiname.find("gebogen") != std::string::npos);
-      if (!bogenweiche) {
-        std::cout << "Keine Bogenweiche\n";
+      if (nurBogenweichen && (signalFrame->Datei.Dateiname.find("gebogen") == std::string::npos)) {
         continue;
       }
 
-      const auto& elementeStrang1 = folgeWeichenstrang(getNachfolger({str_element.get(), norm}, 0));
-      const auto& elementeStrang2 = folgeWeichenstrang(getNachfolger({str_element.get(), norm}, 1));
-
-      std::cout << "Elemente in Strang 1:\n";
-      for (const auto& el : elementeStrang1) {
-        std::cout << el.first->Nr << ", kr=" << (el.second ? el.first->kr : -el.first->kr) << "\n";
-      }
-
-      std::cout << "Elemente in Strang 2:\n";
-      for (const auto& el : elementeStrang2) {
-        std::cout << el.first->Nr << ", kr=" << (el.second ? el.first->kr : -el.first->kr) << "\n";
-      }
-
-      // TODO Herausfinden, welches der abzweigende Strang ist
-      assert(signal->children_MatrixEintrag.size() == 2);
-
-      // TODO Originaldatei herausfinden; Kruemmung des abzweigenden Stranges aus Originaldatei ableiten und zur existierenden Kruemmung addieren
-      for (const auto& it : weichen) {
-        if (signalFrame->Datei.Dateiname.find(it.first) != std::string::npos) {
-          std::cout << it.second << "\n";
-          const auto& st3Original = zusixml::parseFile(zusixml::zusiPfadZuOsPfad(it.second, ""));
-          if (!st3Original || !st3Original->Strecke) {
-            std::cout << "Fehler beim Parsen\n";
-          }
-          break;
-        }
-      }
+      result.push_back(Weiche {
+          signal.get(),
+          { str_element.get(), norm },
+          folgeWeichenstrang(getNachfolger({str_element.get(), norm}, 0)),
+          folgeWeichenstrang(getNachfolger({str_element.get(), norm}, 1)) });
     }
   }
+
+  return result;
+}
+
+int main(int argc, char* argv[]) {
+  const auto& zusi = zusixml::parseFile(argv[1]);
+  if (!zusi || !zusi->Strecke) {
+    return 1;
+  }
+
+  int result = 0;
+  for (const auto& bogenweiche : FindeWeichen(*zusi->Strecke, true)) {
+    std::cout << "\nBogenweiche gefunden an Element " << bogenweiche.startElement.first->Nr << "\n";
+    std::cout << "Erster Signalframe:\n";
+    const auto& dateinameErsterSignalframe = bogenweiche.weichensignal->children_SignalFrame.at(0)->Datei.Dateiname;
+    std::cout << " - " << dateinameErsterSignalframe << "\n";
+
+    std::cout << "Elemente in Strang 1:\n";
+    for (const auto& el : bogenweiche.geraderStrang) {
+      std::cout << " - " << el.first->Nr << ", kr=" << (el.second ? el.first->kr : -el.first->kr) << "\n";
+    }
+
+    std::cout << "Elemente in Strang 2:\n";
+    for (const auto& el : bogenweiche.abzweigenderStrang) {
+      std::cout << " - " << el.first->Nr << ", kr=" << (el.second ? el.first->kr : -el.first->kr) << "\n";
+    }
+
+    // TODO Herausfinden, welches der abzweigende Strang ist
+    assert(bogenweiche.weichensignal->children_MatrixEintrag.size() == 2);
+
+    // TODO Originaldatei herausfinden; Kruemmung des abzweigenden Stranges aus Originaldatei ableiten und zur existierenden Kruemmung addieren
+    bool found = false;
+    for (const auto& it : OriginalWeichen) {
+      if (dateinameErsterSignalframe.find(it.first) != std::string::npos) {
+        std::cout << "Unverbogene Weiche: " << it.second << "\n";
+        const auto& st3Original = zusixml::parseFile(zusixml::zusiPfadZuOsPfad(it.second, ""));
+        if (!st3Original || !st3Original->Strecke) {
+          result = 1;
+          std::cout << "Fehler beim Parsen\n";
+        }
+
+        const auto& originaldateiWeichen = FindeWeichen(*st3Original->Strecke);
+        if (originaldateiWeichen.size() != 1) {
+          std::cout << "Nicht genau eine Weiche gefunden\n";
+          result = 1;
+          continue;
+        }
+
+        found = true;
+        break;
+      }
+    }
+
+    if (!found) {
+      std::cout << "Unverbogene Weiche kann nicht ermittelt werden\n";
+      result = 1;
+    }
+  }
+  return result;
 }
